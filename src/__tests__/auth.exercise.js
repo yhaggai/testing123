a22f16dee26deec5a8b613d98f2db954294f969c
Haggai Yaniv <yhaggai@gmail.com>
2021-03-22 12:40:37 +0200

    change

--- src/__tests__/auth.exercise.js
+++ src/__tests__/auth.exercise.js
@@ -1,18 +1,38 @@
 // Testing Authentication API Routes
 
+
+
+
+// * curl -i -H "Accept:application/json" -H "Content-Type:application/json" -H "Authorization: Bearer ACCESS-TOKEN" -XPOST "https://gorest.co.in/public-api/users" -d '{"name":"Tenali Ramakrishna", "gender":"Male", "email":"tenali.ramakrishna@15ce.com", "status":"Active"}'*/
 // 🐨 import the things you'll need
 // 💰 here, I'll just give them to you. You're welcome
-// import axios from 'axios'
+import axios from 'axios'
 // import {resetDb} from 'utils/db-utils'
-// import * as generate from 'utils/generate'
+import * as generate from 'utils/generate'
+import faker from 'faker'
 // import startServer from '../start'
+const ACCESS_TOKEN = '473e5d0a0eaa2c417d60be0eae60a6892e8c8b7a1d2ffa8e7f9dc5d727bb549f'
+const baseURL = 'https://gorest.co.in/public-api'
+const api = axios.create({baseURL,   headers: {'Authorization': `Bearer ${ACCESS_TOKEN}`}})
+
+function createFakeUser() {
+  return {
+    email:  faker.internet.email(),
+    name: faker.name.findName(),
+    gender: faker.name.gender(),
+    status: faker.random.boolean() ? 'Active' : 'Inactive'
+
+  }
+} 
 
 // 🐨 you'll need to start/stop the server using beforeAll and afterAll
 // 💰 This might be helpful: server = await startServer({port: 8000})
 
 // 🐨 beforeEach test in this file we want to reset the database
 
-test('auth flow', async () => {
+test('create user', async () => {
+  const res = await api.post('users', {name:"Tenali Ramakrishna", gender:"Male", email:"tenali.ra1makrishna@15ce.com", status:"Active"})
+  console.log('users', res.data)
   // 🐨 get a username and password from generate.loginForm()
   //
   // register
